b383d708266315a8a501a011d709ff3264b197a1
Artur Trapp <trapartu@amazon.com>
2020-01-15 16:31:52 +0000

    Testing GPG

---
+++ server/src/controllers/v1/address.controller.ts
@@ -0,0 +1,11 @@
+import Sku from "../../models/v1/sku.model";
+
+export default class AddressController {
+    public get = async (_req: Request, _res: Response) => {
+        try { 
+
+        } catch (e) {
+
+        }
+    }
+}
