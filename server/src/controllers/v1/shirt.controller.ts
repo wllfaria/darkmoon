1398a8f29efe8cddeda18571bb90c4f9b4d7691f
Artur Trapp <trapartu@amazon.com>
2020-01-16 13:10:29 +0000

    Adding address controller and route
    This commit creates the routes for addresses, adds the validations and
    the controllers needed. It is not tested yet.

--- server/src/controllers/v1/shirt.controller.ts
+++ server/src/controllers/v1/shirt.controller.ts
@@ -12,54 +12,55 @@ import SuccessMessage from "../../models/v1/MessageFactory/successMessage";
 import { Database } from "../../database";
 
 export default class ShirtController {
-  public get = async (_req: Request, res: Response) => {
-    try {
-      let results = await Sku.findAll({ include: [ProductType, { model: Shirt, include: [Gender, ProductModel] }, ProductImage] });
-      MessageFactory.buildResponse(SuccessMessage, res, results);
-    } catch (err) {
-      MessageFactory.buildResponse(ErrorMessage, res, err);
-    }
-  }
+	public get = async (_req: Request, res: Response) => {
+		try {
+			let results = await Sku.findAll({ include: [ProductType, { model: Shirt, include: [Gender, ProductModel] }, ProductImage] });
+			MessageFactory.buildResponse(SuccessMessage, res, results);
+		} catch (err) {
+			MessageFactory.buildResponse(ErrorMessage, res, err);
+		}
+	}
 
-  public getByUrl = (req: Request, res: Response) => {
-    const requestValidator: RequestValidator = new RequestValidator();
-    const errors = requestValidator.extractErrors(req);
-    if (errors.length) {
-      MessageFactory.buildResponse(ErrorMessage, res, errors);
-      return;
-    }
-    Shirt.findOne
-  }
+	public getByUrl = (req: Request, res: Response) => {
+		const requestValidator: RequestValidator = new RequestValidator();
+		const errors = requestValidator.extractErrors(req);
+		if (errors.length) {
+			MessageFactory.buildResponse(ErrorMessage, res, errors);
+			return;
+		}
+		// Wtf is this
+		Shirt.findOne
+	}
 
-  public create = async (req: Request, res: Response) => {
+	public create = async (req: Request, res: Response) => {
 
-    const transaction = await Database.getInstance().getTransaction();
-    try {
-      const requestValidator: RequestValidator = new RequestValidator();
-      const errors = requestValidator.extractErrors(req);
-      if (errors.length) {
-        MessageFactory.buildResponse(ErrorMessage, res, errors);
-        return;
-      }
-      const { productName, productUrl, productType, avaliable, price, size, model, gender, images } = req.body;
-      const skuResult: Sku = await Sku.create(
-        {
-          product_name: productName, product_url: productUrl, type_id: productType, avaliable
-        },
-        {
-          transaction
-        });
+		const transaction = await Database.getInstance().getTransaction();
+		try {
+			const requestValidator: RequestValidator = new RequestValidator();
+			const errors = requestValidator.extractErrors(req);
+			if (errors.length) {
+				MessageFactory.buildResponse(ErrorMessage, res, errors);
+				return;
+			}
+			const { productName, productUrl, productType, avaliable, price, size, model, gender, images } = req.body;
+			const skuResult: Sku = await Sku.create(
+				{
+					product_name: productName, product_url: productUrl, type_id: productType, avaliable
+				},
+				{
+					transaction
+				});
 
-      await Shirt.create({ sku_id: skuResult.id, price, size, model_id: model, gender_id: gender }, { transaction });
-      images.forEach(async (image: any) => {
-        await ProductImage.create({ url: image.url, sku_id: skuResult.id, alt: image.alt }, { transaction })
-      });
-      await transaction?.commit();
-      MessageFactory.buildResponse(SuccessMessage, res, { ok: true });
-    } catch (err) {
-      // Rollbacks everything in case of explosion
-      await transaction?.rollback();
-      MessageFactory.buildResponse(ErrorMessage, res, err);
-    }
-  }
+			await Shirt.create({ sku_id: skuResult.id, price, size, model_id: model, gender_id: gender }, { transaction });
+			images.forEach(async (image: any) => {
+				await ProductImage.create({ url: image.url, sku_id: skuResult.id, alt: image.alt }, { transaction })
+			});
+			await transaction?.commit();
+			MessageFactory.buildResponse(SuccessMessage, res, { ok: true });
+		} catch (err) {
+			// Rollbacks everything in case of explosion
+			await transaction?.rollback();
+			MessageFactory.buildResponse(ErrorMessage, res, err);
+		}
+	}
 }
