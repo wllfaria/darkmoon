0239c7f4196e6b8f2538bb78621fa28bfe0847e7
Artur Trapp <trapartu@amazon.com>
2020-01-16 13:10:29 +0000

    Adding address controller and route
    This commit creates the routes for addresses, adds the validations and
    the controllers needed. It is not tested yet.

--- server/src/router.ts
+++ server/src/router.ts
@@ -2,6 +2,7 @@ import * as core from "express-serve-static-core";
 import ShirtController from "./controllers/v1/shirt.controller";
 import RequestValidator from "./validations/v1/requestValidator.validation";
 import PersonController from "./controllers/v1/person.controller";
+import AddressController from "./controllers/v1/address.controller";
 const cors = require('cors')
 
 export default class Router {
@@ -10,20 +11,23 @@ export default class Router {
 	private shirtController: ShirtController;
 	private personController: PersonController;
 	private requestValidator: RequestValidator;
+	private addressController: AddressController;
 
 	constructor(app: core.Express) {
-			this.app = app;
-			this.shirtController = new ShirtController();
-			this.personController = new PersonController();
-			this.requestValidator = new RequestValidator();
+		this.app = app;
+		this.shirtController = new ShirtController();
+		this.personController = new PersonController();
+		this.addressController = new AddressController();
+		this.requestValidator = new RequestValidator();
 	}
 
 	public initializeRoutes = (): void => {
-		this.shirtsRoutes();
-		this.peopleRoutes();
+		this.makeShirtRoutes();
+		this.makePeopleRoutes();
+		this.makeAddressRoutes();
 	}
 
-	private shirtsRoutes = (): void => {
+	private makeShirtRoutes = (): void => {
 		this.app.route('/shirts')
 			.get(cors(), this.shirtController.get)
 			.post(cors(), this.requestValidator.shirtValidator('create'), this.shirtController.create);
@@ -31,11 +35,18 @@ export default class Router {
 			.get(cors(), this.requestValidator.shirtValidator('getbyurl'), this.shirtController.getByUrl)
 	}
 
-	private peopleRoutes = (): void => {
+	private makePeopleRoutes = (): void => {
 		this.app.route('/persons')
 			.post(cors(), this.requestValidator.personValidator('create'), this.personController.create)
 		this.app.route('/persons/auth')
 			.post(cors(), this.requestValidator.personValidator('auth'), this.personController.login)
+	}
+
+	private makeAddressRoutes = (): void => {
+		this.app.route('/addresses')
+			.post(cors(), this.requestValidator.addressValidator('create'), this.addressController.create);
+		this.app.route('/addresses/:id')
+			.get(cors(), this.requestValidator.addressValidator('getbyid'), this.addressController.getById)
 	}
 
 }
