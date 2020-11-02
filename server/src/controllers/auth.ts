2ca19108c5339656641bcfea4803b4d82526c47b
Artur Trapp <artptrapp@hotmail.com>
2020-11-02 07:50:36 -0300

    feat(server): adding authenticate method

---
+++ server/src/controllers/auth.ts
@@ -0,0 +1,41 @@
+import { APIGatewayProxyEvent } from "aws-lambda"
+import { Context } from "vm"
+import { authenticate } from "../models/auth"
+import { checkToken, extractTokenFromHeaders } from "../utils/auth"
+import { parseBody } from "../utils/lambdaUtils"
+import { BadRequest, InternalServerError, OK } from "../utils/lambdaWrapper"
+
+class Auth {
+  public verify(event: APIGatewayProxyEvent, _context: Context) {
+    try {
+      const token = extractTokenFromHeaders(event)
+      const decoded = checkToken(token)
+      if (!decoded || !token) {
+        return BadRequest({ message: 'Invalid or missing token' })
+      }
+      return OK({
+        appName: 'Darkmoon Cloud',
+        token: decoded
+      })
+    } catch (e) {
+      return InternalServerError({ message: e })
+    }
+  }
+
+  public async authenticate(event: APIGatewayProxyEvent, _context: Context) {
+    try {
+      const body = parseBody(event.body)
+      const { username, password } = body
+      if (!username || !password) {
+        return BadRequest({ message: 'Missing credentials' })
+      }
+      const signed = await authenticate(username, password)
+      return OK(signed)
+    } catch (e) {
+      return InternalServerError({ message: e })
+    }
+  }
+}
+
+const handler = new Auth()
+export default handler
--- server/src/handlers/auth.ts
+++ server/src/handlers/auth.ts
@@ -1,57 +1,10 @@
-import * as jwt from 'jsonwebtoken'
-import { Unauthorized } from '../utils/lambdaWrapper'
+import { APIGatewayProxyHandler } from "aws-lambda"
+import authHandler from '../controllers/auth'
 
-const testPrivateKey = "banaan"
-
-const extractToken = (args) => {
-  const functionArgs = args && args[0]
-  if (!functionArgs) {
-    return null
-  }
-
-  const headers = functionArgs && functionArgs.headers
-  if (!headers) {
-    return null
-  }
-
-  const authorization = headers && (headers.Authorization || headers.authorization)
-  if (!authorization) {
-    return null
-  }
-
-  return authorization.replace('JWT', '').replace(' ', '')
+export const checkAuthentication: APIGatewayProxyHandler = async (event, context) => {
+  return authHandler.verify(event, context)
 }
 
-const checkToken = (token: string) => {
-  try {
-    const decodedToken = jwt.verify(token, testPrivateKey)
-    return decodedToken
-  } catch (e) {
-    console.error(`Failed to verify token: ${e.message}`)
-    return null
-  }
+export const authenticate: APIGatewayProxyHandler = async (event, context) => {
+  return authHandler.authenticate(event, context)
 }
-
-/**
- * Decorator that overrides the lambdas
- * and kicks the request out if no or invalid
- * authorization was provided
- */
-export const PrivateLambda = () => {
-  return (_target: Object, _key: string | symbol, descriptor: PropertyDescriptor) => {
-    const original = descriptor.value
-    descriptor.value = (...args) => {
-      const authorization = extractToken(args)
-      if (!authorization) {
-        return Unauthorized('Missing authorization token')
-      }
-
-      const decoded = checkToken(authorization)
-      if (!decoded) {
-        return Unauthorized('Invalid token provided')
-      }
-
-      return original.apply(this, args)
-    }
-  }
-} 
